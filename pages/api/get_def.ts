6badba99b466d6e2c5f6c15cf4f139a3bd15ed1f
Reinaldo Assis <randroi75@gmail.com>
2022-08-20 11:50:57 -0300

    fix: error when parsing end of content

--- pages/api/get_def.ts
+++ pages/api/get_def.ts
@@ -27,9 +27,7 @@ export default async function handler(
 
   let html = await (
     await fetch(
-      `http://${args.base ?? "fr"}.wiktionary.org/w/index.php?title=${
-        args.word ?? "null"
-      }&printable=yes`
+      `http://${args.base ?? "fr"}.wiktionary.org/wiki/${args.word ?? "null"}`
     )
   ).text();
   let root = parse(html);
@@ -41,9 +39,15 @@ export default async function handler(
   //limpa as tabelas do codigo fonte
   root.querySelectorAll("table").forEach((t) => t.remove());
   //pega a posição (se houver) da primeira sectionlanguage que não seja FR TODO: remove hardcoded language
-  let position = root
-    .querySelectorAll(".sectionlangue")
-    .filter((x) => x.id != "fr")[0].innerHTML;
+
+  let position = "";
+  try {
+    position = root
+      .querySelectorAll(".sectionlangue")
+      .filter((x) => x.id != "fr")[0].innerHTML;
+  } catch {
+    position = "</div>";
+  }
 
   let aux_html = root.querySelector(".mw-parser-output")?.innerHTML ?? "";
   let maininfo =
@@ -94,52 +98,7 @@ export default async function handler(
       definitionsMor[
         section.querySelectorAll(".mw-headline").reverse()[0]?.innerText.trim()
       ] = definitions;
-
-      // console.log(
-      //   `${section.querySelectorAll(".mw-headline").reverse()[0]?.innerText}`
-      // );
     }
-
-    console.log("\n----------------\n");
   }
   res.status(200).json({ value: definitionsMor });
-
-  //console.log((maininfo ?? "")[0]);
-
-  //pega o tipo de classe referente a palavra
-  // let wordClass = root.querySelectorAll(".mw-headline")[2]?.text ?? "null";
-  // wordClass = wordClass.trim(); //limpa a string
-
-  // //a parte principal do html onde se encontram as possíveis definições
-  // //e seus exemplos em frases
-  // let definicoesMor = root.querySelector("ol");
-
-  // //uma lista que contem as possíveis definições e suas frases
-  // //filtra para coletar todos os <li> que não tenham parente <ul>
-  // //aqueles com parentes <ul> são exemplos de frases
-  // let exampleElementList =
-  //   definicoesMor
-  //     ?.querySelectorAll("li")
-  //     .filter((x) => x.parentNode.localName != "ul") ?? [];
-
-  // //lista de definições a ser retonada pela API
-  // let definitions: Array<Definition> = [];
-
-  // //caso a lista não for vazia
-  // if (exampleElementList.length > 0) {
-  //   exampleElementList.map((x) => {
-  //     let exampleContent = x.querySelector("ul");
-  //     let examplePhrase = exampleContent?.querySelectorAll("li");
-  //     let phraseList: Array<string> = [];
-  //     examplePhrase?.forEach((x) => phraseList.push(x.text.trim()));
-
-  //     let def: Definition = {
-  //       def: x.text.replace(exampleContent?.text ?? "", ""),
-  //       example: phraseList ?? "",
-  //     };
-  //     definitions.push(def);
-  //   });
-  // }
-
-  //res.status(200).json({ value: definitions, class: wordClass });
 }
